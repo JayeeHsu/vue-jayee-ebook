3d61d40f4961c89e60d5500ffecc25ad8bf4a72f
JayeeHsu <2750089229@qq.com>
2020-12-15 00:53:57 +0800

    2020-12-15 1.书城首页布局

--- src/views/store/StoreHome.vue
+++ src/views/store/StoreHome.vue
@@ -7,28 +7,17 @@
           @onScroll="onScroll"
           ref="scroll"
         >
-         <div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>  <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>  <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>  <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>  <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-           <div>1111111111</div>
-         </div>
+          <div class="banner-wrapper">
+<!--            <img class="banner-img" :src="banner" alt="">-->
+            <div class="banner-img" :style="{backgroundImage:`url('${banner}')`}"></div>
+          </div>
+          <guess-you-like :data="guessYouLike"></guess-you-like>
+          <recommend :data="recommend"></recommend>
+          <featured :data="featured" :title-text="$t('home.featured')" :btn-text="$t('home.seeAll')" class="featured"></featured>
+          <div class="category-list-wrapper" v-for="(item, index) in categoryList" :key="index">
+            <category-book :data="item"></category-book>
+          </div>
+          <category class="categories" :data="categories"></category>
         </scroll>
     </div>
 </template>
@@ -39,6 +28,11 @@ import Scroll from '../../components/common/Scroll'
 import FlapCard from '../../components/home/FlapCard'
 import { storeHomeMixin } from '../../utils/mixin'
 import { home } from '../../api/store'
+import GuessYouLike from '../../components/home/GuessYouLike'
+import Recommend from '../../components/home/Recommend'
+import Featured from '../../components/home/Featured'
+import CategoryBook from '../../components/home/CategoryBook'
+import Category from '../../components/home/Category'
 
 export default {
   name: 'StoreHome',
@@ -46,7 +40,24 @@ export default {
   components: {
     SearchBar,
     Scroll,
-    FlapCard
+    FlapCard,
+    GuessYouLike,
+    Recommend,
+    Featured,
+    CategoryBook,
+    Category
+  },
+  data () {
+    return {
+      scrollTop: 94,
+      random: null,
+      banner: null,
+      guessYouLike: null,
+      recommend: null,
+      featured: null,
+      categoryList: null,
+      categories: null
+    }
   },
   mounted () {
     home().then(response => {
@@ -54,15 +65,16 @@ export default {
         const data = response.data
         const randomIndex = Math.floor(Math.random() * data.random.length)
         this.random = data.random[randomIndex]
+        this.banner = data.banner
+        this.guessYouLike = data.guessYouLike
+        this.recommend = data.recommend
+        this.featured = data.featured
+        this.categoryList = data.categoryList
+        this.categories = data.categories
       }
     })
   },
-  data () {
-    return {
-      scrollTop: 94,
-      random: null
-    }
-  },
+
   methods: {
     /*
     * 监听scroll组件滚动 @onScroll="onScroll"
@@ -88,5 +100,28 @@ export default {
   .store-home {
     width: 100%;
     height: 100%;
+    .banner-wrapper{
+      width: 100%;
+      padding: px2rem(10);
+      box-sizing: border-box;
+      .banner-img{
+        width: 100%;
+        height: 140px;
+        background-size:100% 100%;
+        background-repeat: no-repeat; // 防止屏幕小于图片大小时repeat
+      }
+    }
+    .recommend {
+      margin-top: px2rem(20);
+    }
+    .featured {
+      margin-top: px2rem(20);
+    }
+    .category-list-wrapper{
+      margin-top: px2rem(20);
+    }
+    .categories{
+      margin-top: px2rem(20);
+    }
   }
 </style>
